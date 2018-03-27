07786348f7a90d5b79b77e9ed2c3078442fa33a4
Matt Pasco <matt.pasco@hotmail.com>
2018-03-27 17:53:51 -0400

    updates

--- Matt/kagglePredictSeasonAvgs.py
+++ Matt/kagglePredictSeasonAvgs.py
@@ -13,7 +13,7 @@ class SeasonStats:
         self.teamStats = [[0]] *numTeams
         self.played = [0] *numTeams
 
-    def addGameStat(self,winningTeamStats, losingTeamStats):
+    def addGameStat(self,winningTeamId, losingTeamId, winningTeamStats, losingTeamStats):
         
 
 
@@ -52,19 +52,21 @@ def getRegularSeason():
 
 
 # gets the 28 features we want from a game
+# returns winningID, LosingID, winningStats, losingStats
+
 def getTeamStats(game):
     # we want the stats starting after location and forth
     fieldFeatures = [ header for header in data[0]][7:]
 
-    winningScore = game["Wteam"]
-    losingScore = game["Lteam"]
+    winningScore = game["Wscore"]
+    losingScore = game["Lscore"]
     winningFeatures = [ game[field] for field in fieldFeatures[1:halfFeatures]]
     losingFeatures = [ game[field] for field in fieldFeatures[halfFeatures:]]
 
     winningTeamStats = [winningScore] + winningFeatures
     losingTeamStats = [losingScore] + losingFeatures
 
-    return (winningTeamStats,losingTeamStats)
+    return (game["Wteam"], game["Lteam"],winningTeamStats,losingTeamStats)
 
 
 
